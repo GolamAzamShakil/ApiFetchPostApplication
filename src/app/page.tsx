b7a6be024bfc86e1f6d887db478c61cfb1254214
GolamAzamShakil <golamazam1010@gmail.com>
2025-05-31 00:59:26 +0600

    button relocation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@
 import { motion } from "motion/react";
 import { AuroraBackground } from "@/components/ui/aurora-background";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
-    <div className="px-2">
+    <div className="px-2 pb-10">
     <AuroraBackground className="rounded-md">
       <motion.div
         initial={{ opacity: 0.0, y: 40 }}
@@ -24,9 +25,10 @@ export default function Home() {
         <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
           And explore our inhouse products.
         </div>
-        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
-          <Link href={"/registration"}>Register your shop</Link>
-        </button>
+        {/* <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2"></button> */}
+          <Link href={"/registration"}>
+          <Button variant="default" className="w-fit text-white dark:text-black px-4 py-2">Register your shop</Button>
+          </Link>
       </motion.div>
     </AuroraBackground>
     </div>
